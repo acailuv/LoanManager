4b8a3b3bbd610d97390033d9298b9ab7d9e5f28e
Dave Hong <liangcai.stdnt@gmail.com>
2019-11-09 11:54:21 +0700

    Navigation Drawer

--- app/src/main/java/com/github/acailuv/loanmanager/MainActivity.kt
+++ app/src/main/java/com/github/acailuv/loanmanager/MainActivity.kt
@@ -2,11 +2,33 @@ package com.github.acailuv.loanmanager
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.view.GravityCompat
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupWithNavController
+import com.github.acailuv.loanmanager.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding =
+            DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        val navController = findNavController(R.id.nav_host_fragment)
+        val appBarConfiguration = AppBarConfiguration(navController.graph, binding.drawerLayout)
+        binding.toolbar
+            .setupWithNavController(navController, appBarConfiguration)
+    }
+
+    override fun onBackPressed() {
+        if (binding.drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            binding.drawerLayout.closeDrawer(GravityCompat.START)
+        } else {
+            super.onBackPressed()
+        }
     }
 }
